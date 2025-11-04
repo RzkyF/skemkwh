7308a7e50d9754e3664d193acc9c03b659bf813f
RzkyF_ <rizkifadillah616@outlook.co.id>
2025-11-04 15:12:39 +0700

    ver 1.3

---
+++ public/function.js
@@ -0,0 +1,11 @@
+    document.getElementById('btnKipas').addEventListener('click', function() {
+    if (kipasDaya = 0) {
+        hasilKipas.innerHTML = `Input daya tidak boleh 0`;
+        hasilKipas.style.display = 'block';
+    }
+        const kipasDaya = parseFloat(document.getElementById('kipasDaya').value) || 0;
+        const kwhKipas = (kipasDaya * 6.6 * 365) / 1000;
+        const hasilKipas = document.getElementById('hasilKipas');
+        hasilKipas.innerHTML = `Konsumsi/tahun:<br><b>${kwhKipas} kWh</b>`;
+        hasilKipas.style.display = 'block';
+    });
