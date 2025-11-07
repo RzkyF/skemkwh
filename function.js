e273177de7b8ea95f30411e7eb4c360b82e5c70e
RzkyF_ <rizkifadillah616@outlook.co.id>
2025-11-07 11:03:37 +0700

    update kipas

--- function.js
+++ function.js
@@ -1,11 +1,15 @@
- document.getElementById('btnKipas').addEventListener('click', function() {
-    if (kipasDaya = 0) {
-        hasilKipas.innerHTML = `Input daya tidak boleh 0`;
-        hasilKipas.style.display = 'block';
-    }
-        const kipasDaya = parseFloat(document.getElementById('kipasDaya').value) || 0;
-        const kwhKipas = (kipasDaya * 6.6 * 365) / 1000;
-        const hasilKipas = document.getElementById('hasilKipas');
-        hasilKipas.innerHTML = `Konsumsi/tahun:<br><b>${kwhKipas} kWh</b>`;
-        hasilKipas.style.display = 'block';
+         document.getElementById('btnKipas').addEventListener('click', function() {
+            const kipasDaya = parseFloat(document.getElementById('kipasDaya').value) || 0;
+            const kwhKipas = (kipasDaya * 6.6 * 365) / 1000;
+
+        if (kipasDaya <= 0) {
+            const hasilKipas = document.getElementById('hasilKipas');
+            hasilKipas.innerHTML = `Input daya tidak boleh 0`;
+            hasilKipas.style.display = 'block';
+        } else {
+            const hasilKipas = document.getElementById('hasilKipas');
+            hasilKipas.innerHTML = `Konsumsi/tahun:<br><b>${kwhKipas} kWh</b>`;
+            hasilKipas.style.display = 'block';
+        }
+    
     });
